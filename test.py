85d1a435f220422b5217b590a1939eaddf4a4a75
jorgeCastro-CVJJ <a01707735@tec.mx>
2023-03-13 12:22:17 -0600

    Update_Repo

---
+++ test.py
@@ -0,0 +1,6 @@
+#Esto es un test para probar un ciclo for que haga un cambio de Path junto con el cambio del nombre de los archivos que se generan, además debe de imprimir un valor que se calcula cada vez que se ejecuta el ciclo for
+#Autor: Jorge Castro    
+
+for i in range(13,0,-1):
+    print(i)
+    print("C:/Users/jorge/Desktop/Python/Prueba/Prueba"+str(i)+".txt")
